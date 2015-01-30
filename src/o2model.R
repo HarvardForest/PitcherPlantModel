7fe7269916a87d9ee9f261657ab9caf5c7d91fa8
mkl <matthewklau@fas.harvard.edu>
2015-01-30 10:57:49 -0500

    update

--- src/o2model.R
+++ src/o2model.R
@@ -1,6 +1,12 @@
 ###Decomp model for the Pitcher Plant system
 ##MKLau 16Oct2014
 
+windows <- function(x,w=1){
+  y <- x[(1+w):length(x)]
+  x <- x[1:(length(x)-w)]
+  return(cbind(x,y))
+}
+
 f.A <- function(t=seq(0,1440,by=1),Amax=4,LCP=20,c=100,f=1/1440){
   PAR <- c*sin(2*pi*f*t)
   out <- Amax * (1 - exp(-0.3*(PAR-LCP)))
@@ -20,7 +26,7 @@ simPitcher <- function(days=3,t=seq(0,1440,by=1),x0=0,a0=10,a.max=2,a.min=0,s=10
   a <- a0
   x <- x0
   out <- list()
-  for (d in 1:days){
+  for (d. in 1:days){
     w <- f.w(t=t,w0=w0+wf)  
     for (i in 1:(length(t))){
       n[i] <- (w[i]*x[i]) / (max(w)*Amax)
@@ -29,7 +35,7 @@ simPitcher <- function(days=3,t=seq(0,1440,by=1),x0=0,a0=10,a.max=2,a.min=0,s=10
       x[x < 0] <- 0
     }
     wf <- w[length(t)]
-    out[[d]] <- cbind(t=(t+(1440*(d-1))),x,A,w,n)[-1,]
+    out[[d.]] <- cbind(t=(t+(1440*(d.-1))),x,A,w,n)[-1,]
   }
   return(do.call(rbind,out))
 }

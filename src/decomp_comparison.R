20f095c41c95135d4f65fced3760381648009e44
mklau <matthewklau@fas.harvard.edu>
2015-04-27 15:55:20 -0400

    finished simulation function, now testing...

--- src/decomp_comparison.R
+++ src/decomp_comparison.R
@@ -1,4 +1,6 @@
-###Example: how to use the functions from o2model.R.
+### Comparing Sirota decomp rate to Baiser 2011 decomp rates
+
+source('./o2model.R')
 
 test <- simPitcher()
 
@@ -6,11 +8,21 @@ t <- seq(0,2880,by=1)
 beta.0 <- 0.000001
 inc.beta <- 0.0000001
 k <- 0
-while (round(f.w(2880,beta.0,w0=75),5)>0){
-k <- k + 1
-if (k == 1000){plot(f.w(t,beta.0,w0=75),ylim=c(0,75));k <- 0}
-beta.0 <- beta.0 + inc.beta
+
+## replace with analytical solution
+## while (round(f.w(2880,beta.0,w0=75),5)>0){
+## k <- k + 1
+## if (k == 1000){plot(f.w(t,beta.0,w0=75),ylim=c(0,75));k <- 0}
+## beta.0 <- beta.0 + inc.beta
+## }
+
+solve.b0 <- function(t=2880,w0=75,wt=0.00001){
+    ### Note: t should be the time at which there is an undetectable 
+    ### amount of prey. Thus, wt is a non-zero but undetectable mass.
+    (-1 * (log(wt,base=exp(1))-log(w0,base=exp(1)))) / t
 }
+
+beta.0 <- solve.b0(w0=75) # wasp in micrograms
 
 plot(f.w(t,beta=beta.0,w0=75),pch=1,cex=0.25,ylim=c(0,75),xlim=c(0,(1440*15)))
 lines(f.w(seq(0,(1440*15)),beta=beta.0,w0=75))
@@ -27,7 +39,8 @@ for (i in 1:100){
 plot(test~seq(1,100,by=1))
 
 w <- f.w(t)
-
+plot(w)
+abline(v=c(1440,(1440*2)))
 
 ###Optimize using Baiser 2011
 ##Only use bacteria only pitchers
@@ -47,17 +60,20 @@ w <- f.w(t)
 ## MassI: initial mass (unit: milligram / missing value: NA)
 ## MassF: final mass (unit: milligram / missing value: NA)
 
-decomp <- read.csv('./hf169-02-decomp.csv')
-fweb <- read.csv('./hf169-01-change.csv')
-d. <- decomp[fweb[,2]==0,] #delimit to bacteria only pitchers
-d.[,2:3] <- d.[,2:3] * 1000 #convert mg to g
+decomp <- read.csv('http://harvardforest.fas.harvard.edu/data/p16/hf169/hf169-02-decomp.csv')
+fweb <- read.csv('http://harvardforest.fas.harvard.edu/data/p16/hf169/hf169-01-change.csv')
+decomp <- decomp[fweb[,2]>=0,] # delimit to food web size
+decomp[,2:3] <- decomp[,2:3] * 1000 #convert mg to ug
                                         #
-##get optimal beta value
-i <- 7
-plot(fp.w(R0=d.[i,2],beta=-0.01,x=x))
-points(1440,d.[i,3],pch='x',col='red')
+## Initial to final mass comparison
+plot(decomp[,3]~decomp[,2])
+
+### Fit the curve using the data
+b0.fit <- apply(decomp,1,function(x) solve.b0(1440*14,x[2],x[3]))
+b0.fit <- b0.fit[b0.fit!=Inf]
+plot(b0.fit)
+hist(b0.fit,xlim=range(c(b0.fit,beta.0)))
+abline(v=mean(b0.fit))
+abline(v=beta.0,lty=2)
 
 
-plot((decomp[,2]*1000),((decomp[,2]-decomp[,3])*1000/(decomp[,2]*1000))*100,xlab='Initial Prey (g)',ylab='Percent Prey Remaining')
-abline(lm(I((decomp[,2]-decomp[,3])*1000/(decomp[,2]*1000)*100)~I(decomp[,2]*1000)))
-summary(lm(I((decomp[,2]-decomp[,3])*1000/(decomp[,2]*1000)*100)~I(decomp[,2]*1000)))
--- src/o2model.R
+++ src/o2model.R
@@ -12,25 +12,26 @@ f.w <- function(t=seq(0,1440,by=1),beta=0.005737,w0=75){
   w0 * exp(-beta * t)
 }
 
-simPitcher <- function(days=3,t=seq(0,1440,by=1),x0=0,a0=10,a.max=2,a.min=0,s=10,d=0.5,m=1,w0=100,Kw=0.001){
-  wf <- 0
-  Amax <- 4
-  A <- 0
-  n <- 0
-  a <- a0
-  x <- x0
-  out <- list()
-  for (d in 1:days){
-    w <- f.w(t=t,w0=w0+wf)  
-    for (i in 1:(length(t))){
-      n[i] <- (w[i]*x[i]) / (max(w)*Amax)
-      A[i] <- f.A(t=t[i],Amax=(Amax+(Amax*n[i])))
-      if (i != length(t)){x[i+1] <- A[i] - m + (w[i] / (Kw + w[i]))}
-      x[x < 0] <- 0
+simPitcher <- function(days=3,t=seq(0,1440,by=1),x0=0,a0=10,a.max=2,a.min=0,s=10,d=0.5,m=1,w0=100,prey.t=720,Kw=0.001,prey.time=720){
+    wf <- 0
+    A <- 0
+    n <- 0
+    a <- a0
+    x <- x0
+    out <- list()
+    for (d in 1:days){
+        w <- f.w(t=t,w0=wf+w0)
+        if (prey.time != 0){w <- c(rep(wf,prey.time),w);w <- w[1:length(t)]}
+        for (i in 1:(length(t))){
+            n[i] <- (w[i]*x[i]) / 100 # arbitrary constant
+            Amax <- (a.max-a.min) / (1+exp(s*n[i]-d)) + a.min
+            A[i] <- f.A(t=t[i],Amax=Amax)
+            if (i != length(t)){x[i+1] <- A[i] - m + (w[i] / (Kw + w[i]))}
+            x[x < 0] <- 0
+        }
+        out[[d]] <- cbind(t=(t+(1440*(d-1))),x,A,w,n)[-1,]
+        wf <- w[length(w)]
     }
-    wf <- w[length(t)]
-    out[[d]] <- cbind(t=(t+(1440*(d-1))),x,A,w,n)[-1,]
-  }
-  return(do.call(rbind,out))
+    return(do.call(rbind,out))
 }
 

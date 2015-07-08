ed0cbfa70308d263f4b8c90c4699adfc9a3cb296
mklau <matthewklau@fas.harvard.edu>
2015-07-08 13:29:09 -0400

    udpate

--- shiny/shiny.R
+++ shiny/shiny.R
@@ -8,4 +8,5 @@ runApp()
 ### Run on shiny.io
 shinyapps::setAccountInfo(name='bitecology', token='6AA1D337343DD81FC75261BCB0CB252A', secret='wSQQh2qMiHPYWTNvMBLjW5UDdQ6a3uJ6UzR7h4xt')
 shinyapps::deployApp()
+
 ### shinyapps::terminateApp('ppSim')

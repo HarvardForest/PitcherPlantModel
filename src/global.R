5c928b2e54ccbd43c4d4969acdfc77ff687e55d9
mklau <matthewklau@fas.harvard.edu>
2016-01-22 18:34:18 -0500

    debug - working on timing

--- src/global.R
+++ src/global.R
@@ -14,87 +14,75 @@ photo <- function(days=3,Amax=4,Amin=0,Aqe=0.3,LCP=0,start=0,amp=50){
     return(out)
 }
 
-pitcherPlantSim <- function(days=3, feedingTime=720, foodWeight=0, beta=0.001, k=1, Bscaler=10,m=0,ai=7.5,aMax=10, aMin=1, s=10, d=1, c=100) {
 
-if (length(foodWeight) < days){
-    foodWeight <- rep(foodWeight,days)[1:days]
-}
+pitcherPlantSim <- function(days=3, feedingTime=720, foodWeight=0, beta=0.001, k=1, Bscaler=10,m=0,aMax=10, aMin=1, s=10, d=1, c=100,x0=0,w0=0,a0=1){
 
-## Initialization ##
-
-# simulate photosynthesis as fixed values
-P <- photo(days)
-# initial nutrient value
-n <- 0
-# initial augmentation value
-a <- ai * ((aMax-aMin)/(1+exp((-s*n[length(minute)])-d)))+aMin
-# initial biological o2 demand
-B <- 0/(k+0)
-# augmented photosynthesis initialization
-A <- a * P[1]
-# o2 at minute=0, P=0 b/c unable to index at minute=0
-x <- A - m*B
-
-# initialize the time keeper
-minute <- 1
-
-for(z in 1:days){
-
-  # run simulation for a full day
-  for(j in 1:1440){
-
-    # adjust biological o2 demand
-    B <- c(B, (w[length(minute)]/(k+w[length(minute)]))*Bscaler)
-
-    # adjust amount of nutrients
-    n <- c(n, (w[length(minute)]*x[length(minute)-1])/c)
-
-    # adjust augmentation value
-    a <- c(a, ((aMax-aMin)/(1+exp((-s*n[length(minute)])-d)))+aMin)
-
-    # update A
-    A <- c(A,(a[length(minute)] * P[length(minute)]))
-
-    # adjust o2 amount
-    tempO2 <- (A[length(minute)])- (m + B[length(minute)])
-
-  
-    if(is.na(tempO2) == FALSE && tempO2 > 0){
-      x <- c(x, tempO2)
-    }
-    else{
-      x <- c(x, 0)
+    if (length(foodWeight) < days){
+        foodWeight <- rep(foodWeight,days)[1:days]
     }
 
-    # adjust minute
-    minute <- c(minute, length(minute)+1)
+    ## Initialization ##
+                                        # time keeper
+    minute <- 1
+                                        # o2 at minute=0, P=0 b/c unable to index at minute=0
+    x <- x0
+                                        # simulate photosynthesis as fixed values
+    P <- photo(days)
+                                        # food weight at time 1
+    if (feedingTime == 1){
+        w <- w0 + foodWeight[1]
+    }else{w <- w0}
+                                        # initial nutrient value
+    n <- w[1] * x[1] / c
+                                        # initial augmentation value
+    a <- a0 * (((aMax-aMin)/(1+exp((-s*n[length(minute)])-d))) + aMin)
+                                        # initial biological o2 demand
+    B <- w[1]/(k+w[1])
+                                        # augmented photosynthesis initialization
+    A <- a * P[1]
 
-    if(j < 1440){
-        ## adjust amount of food
-        w <- c(w, w[length(w)]*exp(-beta*(1)))
-    }
-    if (j == feedingTime){
-        ## add food
-        w <- c(w, w[length(w)] + foodWeight[z])
-    }else{w <- c(w , w[length(w)])}
-  }
-}
-
-# trim objects to appropriate time
-  # omitted values aren't relevant
-minute <- minute[1:length(P)]
-A <- A[1:length(P)]
-B <- B[1:length(P)]
-n <- n[1:length(P)]
-a <- a[1:length(P)]
-x <- x[1:length(P)]
-w <- w[1:length(P)]
-
-data <- data.frame(minute, P, x, A, B, n, a, w)
-colnames(data) <- c("Minute", "PAR","Oxygen", "Photosynthesis",
-                    "Biological Oxygen Demand", "Nutrients",
-                    "Augmentation Value", "Food Amount")
-return(data)
+                                        #start day loop
+    for (z in 1:days){
+                                        #star minute loop
+        for (j in 1:1440){
+            print(c(z,j,minute[length(minute)]))
+            ##update##
+                                        # time keeper
+            minute <- c(minute,minute[length(minute)] + 1)
+                                        # food weight at time 1
+            wt <- w[length(minute) - 1] * exp(w[length(minute) - 1] * beta)
+            if (minute[length(minute)] == feedingTime){
+                w <- c(w,wt + foodWeight[z])
+            }else{
+                w <- c(w,wt)
+            }
+                                        # nutrient value
+            n <- c(n,w[length(minute)] * x[length(minute) - 1] / c)
+                                        # augmentation value
+            a <- c(a,a[length(minute) - 1] * (((aMax - aMin)/(1 + exp((-s * n[length(minute)]) - d))) + aMin))
+                                        # biological o2 demand
+            B <- c(B,w[length(minute)] / (k + w[length(minute)]) * Bscaler)
+                                        # augmented photosynthesis initialization
+            A <- c(A,a[length(minute)] * P[length(minute)])
+                                        # o2 at minute=0, P=0 b/c unable to index at minute=0
+            x <- c(x, A[length(minute)] - m + B[length(minute)])
+        } # end minute loop
+    } # end day loop
+                                        # trim objects to appropriate time
+                                        # omitted values aren't relevant
+    minute <- minute[1:length(P)]
+    A <- A[1:length(P)]
+    B <- B[1:length(P)]
+    n <- n[1:length(P)]
+    a <- a[1:length(P)]
+    x <- x[1:length(P)]
+    w <- w[1:length(P)]
+                                        # prep for export
+    data <- data.frame(minute, x, P, A, B, w, n, a)
+    colnames(data) <- c("Minute", "Oxygen", "PAR","Photosynthesis",
+                        "Biological Oxygen Demand", "Food Amount", "Nutrients",
+                        "Augmentation Value")
+    return(data)
 }
 
 maxify <- function(x,p=1440){
@@ -124,24 +112,24 @@ ppHyst <- function(x,n1,n2,feedingTime=720,tol=0){
     hyst <- x[hyst.start:length(x)]
     max.hyst <- maxify(hyst)
 
-## max - base
+    ## max - base
     dMB <- max(max.hyst) - base
 
-## min - base
+    ## min - base
     dmB <- min(max.hyst) - base
 
-## return rate = time from last feeding to return to base
+    ## return rate = time from last feeding to return to base
     if (all(max.hyst <= (base + tol) &  max.hyst >= (base - tol))){
         r.t <- ((1:length(max.hyst))[max.hyst <= (base + tol) & 
                                          max.hyst >= (base - tol)][1])
     }else{
         r.t <- length(max.hyst)
     }
-    
+
     Mrr <- (dMB) / r.t
     mrr <- (dmB) / r.t
 
-## output
+    ## output
     out <- c(return.time=r.t,dMB=dMB,max.return.rate=Mrr,dmB=dmB,min.return.rate=mrr)
     return(out)
 }

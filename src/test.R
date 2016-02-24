07597ec6a109fe80ce31f8f43abf0583040619e3
mklau <matthewklau@fas.harvard.edu>
2016-02-24 10:47:46 -0500

    added test.R

---
+++ src/test.R
@@ -0,0 +1,17 @@
+### testing the funcitons for the pitcher plant model
+
+### relations between variables
+
+
+## decomposition and time
+
+## prey and decomposition
+
+## prey and nitrogen
+
+## nitrogen and augmentation
+
+## augmentation and oxygen
+
+## lag plot of nitrogen and oxygen
+

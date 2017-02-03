aa99fd2c6679ed8972e24c8473636cf3b92eef96
mklau <matthewklau@fas.harvard.edu>
2017-02-03 10:48:37 -0500

    adds decomposition and correct data parsing to sirota

--- src/sirota2014.R
+++ src/sirota2014.R
@@ -7,27 +7,21 @@ library(txtplot)
 ### Import from the Harvard Forest Archive
 ### Data should include the daily trend
 
+
 hf205 <- read.csv('http://harvardforest.fas.harvard.edu/data/p20/hf205/hf205-01-TPexp1.csv')
-hf205 <- split(hf205,hf205$run.num)
-dat <- hf205[[1]]
+summary(hf205)
+hf205 <- split(hf205,hf205$variable)
+hf205 <- lapply(hf205,function(x) split(x,f = x$run.num))
+
+## "air.temp"   "control"    "high"       "low"        "med.high"  
+## "med.low"    "par"        "water.temp"
+
+dat <- hf205[['high']][[4]]
 dat$datetime <- as.numeric(dat$datetime)
 dat$datetime <- order(dat$datetime)
-
-attach(dat)
-txtplot(datetime,value.i,pch='.')
-
-ts.x <- ts(value.i,frequency = 1440)
-txtplot(ts.x)
-
+ts.x <- ts(dat$value.i,frequency = 1440)
 tsd.x <- decompose(ts.x)
-names(tsd.x)
-txtplot(tsd.x$seasonal)
-txtplot(tsd.x$trend)
-txtplot(tsd.x$random)
-
-txtdensity(na.omit(tsd.x$random))
-
-detach(dat)
+plot(tsd.x)
 
 ### Check for model fit
 

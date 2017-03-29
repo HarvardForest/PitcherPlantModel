3ad49cf818d8ec647884736606652418d2b95f67
mklau <matthewklau@fas.harvard.edu>
2017-03-29 11:31:45 -0400

    updates rss.min

--- src/global.R
+++ src/global.R
@@ -272,7 +272,7 @@ ddsoSim <- function(days,fW,beta,k){
 
 min.rss <- function(data,par){
     with(data, sum((ddso -  
-                        ddoSim(days,
+                        ddsoSim(days,
                                fW,
                                beta = par[1],
                                k = par[2])[1:length(ddso)])^2))
